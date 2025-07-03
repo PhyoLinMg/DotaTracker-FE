5c753528f9f7f78c11f4d22abfb2cc758235743e
PhyoLinMg <gangsterplm@gmail.com>
2025-07-03 18:20:06 +0700

    minor fix on the UI

--- services/client/axios_client.ts
+++ services/client/axios_client.ts
@@ -7,7 +7,7 @@ class AxiosClient {
 
   private constructor() {
     this.axiosInstance = axios.create({
-      baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api',
+      baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3356/api',
       timeout: 10000,
       headers: {
         'Content-Type': 'application/json',
